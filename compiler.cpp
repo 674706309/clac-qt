51b164584b5e8b62b6d3f94382496de3aeae6473
674706309 <674706309@qq.com>
2018-01-16 21:43:57 +0800

    create getch()

--- compiler.cpp
+++ compiler.cpp
@@ -3,11 +3,30 @@
 Compiler::Compiler(QString str)
 {
     exp=str;
+    length=-1;
     getCh();
     ans="0";
 }
 
 int Compiler::getCh()
 {
+    QChar buf[20];
+    int pos=0;
+    QChar ch=exp.at(++length);
+    while (ch==' '||ch=='\t'||ch=='\r'||ch=='\n') {
+        if(length>=exp.length()){
+            return -1;
+        }
+        ch=exp.at(++length);
+    }
+    if(isalpha(ch)){
+        while (isalpha(ch)||isdigit(ch)) {
+            buf[pos++]=ch;
+            length++;
+            ch=exp.at(length);
+        }
+
+    }
+
     return 0;
 }
